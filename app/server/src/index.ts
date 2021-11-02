a9c2f61e91cc0de98a2de5a53c6adf20cfa1e4af
haidaroktafiansyah <haidaroktafiansyah@gmail.com>
2021-11-02 18:30:05 +0700

    try email validatior

--- app/server/src/index.ts
+++ app/server/src/index.ts
@@ -1,1 +1,31 @@
-export const ext = {}
+//mailer
+export const nodemailer = require("nodemailer");
+
+//uniquie string maker
+const uuidv4 = require('uuid');
+
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "andromediarecruitment@gmail.com",
+        pass: "team41234!",
+    },
+});
+
+transporter.verify((error, success) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("email verifier is ready");
+        console.log(success);
+    }
+});
+
+//validator
+const Joi = require('joi');
+
+// password hasher
+const bcrypt = require('bcryptjs');
+
+export const ext = { transporter, Joi, bcrypt }
--- app/web/src/global.ts
+++ app/web/src/global.ts
@@ -1,35 +1,4 @@
 
-//validator
-export const Joi = require('joi');
-
-// password hasher
-export const bcrypt = require('bcryptjs');
-
-//mailer
-const nodemailer = require("nodemailer");
-
-//uniquie string maker
-const { uuid } = require('uuidv4');
-
-
-export const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: 'andromediarecruitment@gmail.com',
-        pass: 'team41234!'
-
-    }
-});
-
-transporter.verify((error, success) => {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log("ready");
-        console.log(success);
-    }
-});
-
 export const ext = {
 
 }
