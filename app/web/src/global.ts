dcf0a61f864c180fbd76d3d6304b45ea5f9b3e7e
haidaroktafiansyah <haidaroktafiansyah@gmail.com>
2021-11-02 09:23:56 +0700

    trynodemailer

--- app/web/src/global.ts
+++ app/web/src/global.ts
@@ -1,8 +1,34 @@
 
+//validator
 export const Joi = require('joi');
 
 // password hasher
 export const bcrypt = require('bcryptjs');
+
+//mailer
+const nodemailer = require("nodemailer");
+
+//uniquie string maker
+const { uuid } = require('uuidv4');
+
+
+export const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: 'andromediarecruitment@gmail.com',
+        pass: 'team41234!'
+
+    }
+});
+
+transporter.verify((error, success) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("ready");
+        console.log(success);
+    }
+});
 
 export const ext = {
 
